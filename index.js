f334fa2c33671b221f812363ce4d18c26e59e720
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-08-16 20:37:37 -0300

    Render elements to the screen

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const test = 'BLA'
+function neymar(type, props, ...children) {
+    props = props || {}
+    return {
+        type,
+        props,
+        children: Array.prototype.concat.apply([], children)
+    }
+}
+
+function createElement(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node)
+    }
+
+    const el = document.createElement(node.type)
+    node.children
+        .map(createElement)
+        .map(el.appendChild.bind(el))
+
+    return el
+}
+
+function view() {
+    return (<div id="frase-reflexiva"><h1>Neymar é fera</h1></div>)
+}
+
+function render(node) {
+    node.appendChild(createElement(view()))
+}
