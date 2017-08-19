d135674646f00284e7ba9e531d6f2005c53a7a9b
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-08-19 11:10:11 -0300

    Add update routine

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function neymar(type, props, ...children) {
     }
 }
 
+function changed(one, two) {
+    return typeof one !== typeof two ||
+        typeof one === 'string' && one !== two ||
+        one.type !== two.type
+}
+
 function createElement(node) {
     if (typeof node !== 'object') {
         return document.createTextNode(node)
@@ -26,14 +32,53 @@ function createElement(node) {
     return el
 }
 
+function updateElement(parent, newNode, oldNode, index = 0) {
+    if (!oldNode) {
+        parent.appendChild(createElement(newNode))
+    } else if (!newNode) {
+        parent.removeChild(parent.childNodes[index])
+    } else if (changed(newNode, oldNode)) {
+        parent.replaceChild(
+            createElement(newNode),
+            parent.childNodes[index]
+        )
+    } else if (newNode.type) {
+        const newLength = newNode.children.length;
+        const oldLength = oldNode.children.length;
+        for (let i = 0; i < Math.max(newLength, oldLength); i++) {
+          updateElement(
+            parent.childNodes[index],
+            newNode.children[i],
+            oldNode.children[i],
+            i
+          );
+        }
+    }
+}
+
 function view(x) {
     return (<div id="frase-reflexiva" className="container">
         <h1 className="cabecalho-top">O ousado chegou</h1>
         <h2 className="nome-do-app-topzera">Relógio de salários do Neymar</h2>
-        <p>Desde que você entrou nessa página, Neymar ganhou {x} dinheiros.</p>
+        <p>Desde que você entrou nessa página, Neymar ganhou {String(x)} dinheiros.</p>
     </div>)
 }
 
+
 function render(node) {
-    node.appendChild(createElement(view(0)))
+    //node.appendChild(createElement(currentView))
+    let currentGolpinhos = 0;
+    let currentView = view(currentGolpinhos);
+
+    updateElement(node, currentView)
+
+    setInterval(() => {
+        const newGolpinhos = Number(currentGolpinhos) + 3.5
+        const newView = view(newGolpinhos)
+        updateElement(node, newView, currentView)
+
+        currentGolpinhos = newGolpinhos
+        currentView = newView
+    }, 1000)
 }
+
